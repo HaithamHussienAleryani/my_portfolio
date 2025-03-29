3535360498c16d652766590b0cdd495954d3d5e8
haithamaleryani <haithamaleryani00@gmail.com>
2025-03-30 01:50:59 +0300

    fix: header bg color

--- src/components/hero-header.tsx
+++ src/components/hero-header.tsx
@@ -27,7 +27,7 @@ export const HeroHeader = () => {
             <nav
                 data-state={menuState && 'active'}
                 className="fixed z-20 w-full px-2">
-                <div className={cn('mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12', isScrolled && 'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5')}>
+                <div className={cn('mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12', isScrolled && 'bg-primary/20 max-w-4xl rounded-2xl  backdrop-blur-lg lg:px-5')}>
                     <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
                         <div className="flex w-full justify-between lg:w-auto">
                             <Link
@@ -80,7 +80,7 @@ export const HeroHeader = () => {
                                 <Button
                                     asChild
                                     size="lg"
-                                    className={cn(isScrolled && 'lg:hidden','rounded-full bg-black')}>
+                                    className={cn(isScrolled && 'lg:hidden','rounded-full hover:text-black bg-black')}>
                                     <Link href="#">
                                         <span>Contact Me</span>
                                     </Link>
@@ -88,7 +88,7 @@ export const HeroHeader = () => {
                                 <Button
                                     asChild
                                     size="lg"
-                                    className={cn(isScrolled ? 'lg:inline-flex ' : 'hidden' , ' rounded-full bg-black')}>
+                                    className={cn(isScrolled ? 'lg:inline-flex ' : 'hidden' , ' rounded-full hover:text-black bg-black')}>
                                     <Link href="#">
                                         <span>Contact Me</span>
                                     </Link>
