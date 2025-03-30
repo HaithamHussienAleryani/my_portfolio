9bc783dfb6b8633b8d9a311b6a97bb04f1c46bd3
haithamaleryani <haithamaleryani00@gmail.com>
2025-03-30 23:18:45 +0300

    feat: fix mobile version

--- src/components/featured-proeject-card.tsx
+++ src/components/featured-proeject-card.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, {useRef} from 'react';
 import {cn} from "@/lib/utils";
 import ProjectImage from '../../public/project.jpg';
 import Image from "next/image";
@@ -7,37 +7,49 @@ import Link from "next/link";
 import {motion, useScroll, useTransform} from 'motion/react';
 
 type props = {
-    startPixel: number;
-    endPixel: number;
     small?: boolean;
 }
 
-const FeaturedProjectCard = ({startPixel,endPixel,small=false}:props) => {
-    const { scrollY } = useScroll();
+const FeaturedProjectCard = ({small = false}: props) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const {scrollYProgress} = useScroll(
+        {
+            target: ref,
+            offset: ["0 1", "1 1"],
+        }
+    );
 
 
-    const scale = useTransform(scrollY,[startPixel,endPixel],[0.7,1]);
+    const scale = useTransform(scrollYProgress, [0, 0.9], [0.7, 1]);
 
     return (
-        <Link href="/" className={'z-10'}>
-        <div className={cn("border pt-20 pb-5 hover:bg-sidebar-primary duration-300  border-white/15 bg-black ")}>
-            <div className={'flex px-20 gap-x-8 text-white items-center'}>
-                <p className={'text-xl tracking-tighter'}> <span className={'project-title-brackets'}>{"{"}</span> Project Title One <span className={'project-title-brackets'}>{"}"}</span></p>
-                     <span className={'text-muted-foreground pt-0.5 font-semibold'}>25/02/13</span>
-            </div>
-            <div className={'px-20'}>
-                <h2 className={'text-white mt-4 tracking-tighter text-3xl font-light'}>Organization name</h2>
-                <p className={'mt-4 text-muted-foreground text-lg tracking-tighter'}>Web Design & Web Development</p>
-            </div>
+        <Link href="/" className={'z-10 bg-black'}>
+            <div className={cn("border pt-8 md:pt-20 pb-5 hover:bg-sidebar-primary duration-300  border-white/15  ")}>
 
-            <motion.div className={'px-5'} style={
-                {
-                    scale,
-                }
-            } >
-                <Image src={ProjectImage} alt={"Project Image"} className={ cn(!small&&"h-[500px]", small&&"h-[350px]" , "rounded-xl mt-5  object-cover w-full")  } />
-            </motion.div>
-        </div>
+
+                <div className={'flex px-8 md:px-20 gap-x-8 text-white items-center'}>
+                    <p className={'text-xl tracking-tighter'}><span className={'project-title-brackets'}>{"{"}</span> Project
+                        Title
+                        One <span className={'project-title-brackets'}>{"}"}</span></p>
+                    <span className={'text-muted-foreground pt-0.5 font-semibold'}>25/02/13</span>
+                </div>
+                <div className={'px-8 md:px-20'}>
+                    <h2 className={'text-white mt-4 tracking-tighter text-3xl font-light'}>Organization name</h2>
+                    <p className={'mt-4 text-muted-foreground text-lg tracking-tighter'}>Web Design & Web Development</p>
+                </div>
+
+
+                <motion.div ref={ref} className={'px-5'} style={
+                    {
+                        scale,
+                    }
+                }>
+                    <Image src={ProjectImage} alt={"Project Image"}
+                           className={cn(!small && " h-[300px] md:h-[500px]", small && " h-[300px] md:h-[350px]", "rounded-xl mt-5  object-cover w-full")}/>
+                </motion.div>
+
+
+            </div>
         </Link>
     );
 };
--- src/components/featured-projects-section.tsx
+++ src/components/featured-projects-section.tsx
@@ -5,27 +5,29 @@ import PrimaryButton from "@/components/PrimaryButton";
 import FeaturedProjectCard from "@/components/featured-proeject-card";
 
 
-
 const FeaturedProjectsSection = () => {
     return (
         <section className="featured-projects">
-            <Image src={OutlinedLogo} className={'featured-projects-logo'} alt={'featured projects image outlined logo'} />
-            <div className={'max-w-6xl mx-auto'}>
-                <div className={'flex justify-start  pt-4 gap-x-2 items-center lg:pb-6'}>
+            <Image src={OutlinedLogo} className={'featured-projects-logo'} alt={'featured projects image outlined logo'}/>
+            <div className={' md:max-w-6xl md:mx-auto'}>
+                <div className={'flex justify-start px-6 md:px-0 pt-4 gap-x-2 items-center pb-3 lg:pb-6'}>
                     <div className={'size-3 rounded-full shadow-[0px_2px_13px_0px_rgba(130,255,31,0.5)] bg-primary'}></div>
-                    <p className={'text-muted-foreground font-semibold'}> 01 - Featured Projects</p>
+                    <p className={'text-muted-foreground text-sm md:text-md font-semibold'}> 01 - Featured Projects</p>
 
                 </div>
-                <h1 className={'text-8xl font-thin max-w-4xl text-white'}>I blend creativity with technical expertise</h1>
-                <PrimaryButton className={'mt-16 '} title={"Let's work together"}/>
+                <h1 className={' text-4xl md:text-8xl px-6 md:px-0   md:font-thin max-w-4xl text-white'}>I blend
+                    creativity with
+                    technical
+                    expertise</h1>
+                <PrimaryButton className={'mt-16 mx-6 md:mx-0 '} title={"Let's work together"}/>
             </div>
-            <div className={'mt-56 grid md:grid-cols-2 '}>
-                <FeaturedProjectCard startPixel={1150} endPixel={1700}/>
-                <FeaturedProjectCard startPixel={1150} endPixel={1700}/>
+            <div className={' mt-10 md:mt-56 grid md:grid-cols-2 '}>
+                <FeaturedProjectCard/>
+                <FeaturedProjectCard/>
             </div>
             <div className={' grid md:grid-cols-3 '}>
-                <FeaturedProjectCard small startPixel={1800} endPixel={2200}/>
-                <FeaturedProjectCard small startPixel={1800} endPixel={2200}/>
+                <FeaturedProjectCard small/>
+                <FeaturedProjectCard small/>
             </div>
         </section>
     );
