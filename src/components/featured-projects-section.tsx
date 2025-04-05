378a458ef5e6ea233a3330162be9d54f8e7d8b64
haithamaleryani <haithamaleryani00@gmail.com>
2025-04-05 21:03:07 +0300

    fix: add arrow to view all projects

--- src/components/featured-projects-section.tsx
+++ src/components/featured-projects-section.tsx
@@ -3,6 +3,7 @@ import PrimaryButton from "@/components/PrimaryButton";
 import FeaturedProjectCard from "@/components/featured-proeject-card";
 import FeaturedBackgroundImage from "@/components/ui/featured-bg";
 import Link from "next/link";
+import {ArrowUpRight} from "lucide-react";
 
 
 const FeaturedProjectsSection = () => {
@@ -28,10 +29,18 @@ const FeaturedProjectsSection = () => {
                 <FeaturedProjectCard small/>
                 <FeaturedProjectCard small/>
                 <Link href={'#'}
-                      className={' bg-primary h-72  text-center flex flex-col justify-center text-black lg:h-auto text-xl md:m-6 md:rounded-2xl hover:rounded-none hover:m-0 hover:bg-primary duration-500'}>
-                    <p>View
-                        all
-                        projects</p>
+                      className={' bg-primary h-[250px]  text-center group/arrow  flex flex-col  justify-center text-black lg:h-auto text-xl md:m-6 md:rounded-2xl hover:rounded-none hover:m-0 hover:bg-primary duration-500'}>
+                    <div className={'flex justify-center items-center space-x-5'}>
+                        <p>View
+                            all
+                            projects</p>
+                        <div
+                            className={'bg-white rounded-full p-2 group-hover/arrow:-translate-x-2 transition-all duration-500 text-black'}>
+                            <ArrowUpRight
+                                className={'size-6 group-hover/arrow:rotate-45 transition-all ease-in-out duration-300'}/>
+                        </div>
+                    </div>
+
                 </Link>
             </div>
         </section>
