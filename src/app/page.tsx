8c8777af10b8d3f08f8b032e0bd8e6c23e92ad7a
Haitham Hussien Aleryani <81069558+HaithamHussienAleryani@users.noreply.github.com>
2025-05-15 14:39:19 +0300

    update strudio client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,30 @@
 import HeroSection from "@/components/hero-section";
-
-import { Lenis } from "lenis/react";
+import {client} from "@/sanity/lib/client";
+import {PORTFOLIO_QUERY} from "@/sanity/queries";
+import {PortfolioContent} from "@/data/portfolioContent";
+import {Lenis} from "lenis/react";
 import StaticsSection from "@/components/statics-section";
-import { HeroHeader } from "@/components/hero-header";
+import {HeroHeader} from "@/components/hero-header";
 import React from "react";
 import FeaturedProjectsSection from "@/components/featured-projects-section";
 import TechStackSection from "@/components/tech-stack-section";
-import WhatStandsMeOutSection from "@/components/ui/how-it-works-section";
 
-//const options = { next: { revalidate: 30 } };
+
+const options = {next: {revalidate: 30}};
 export default async function Home() {
-//   const portfolio = await client.fetch<PortfolioContent[]>(
-//     PORTFOLIO_QUERY,
-//     {},
-//     options
-//   );
 
-//   console.log(portfolio);
+    const portfolio = await client.fetch<PortfolioContent[]>(PORTFOLIO_QUERY, {}, options);
 
-  return (
-    <Lenis root>
-      <HeroHeader />
-      <HeroSection />
-      <StaticsSection />
-      <FeaturedProjectsSection />
-      <TechStackSection />
-      <StaticsSection />
-      <WhatStandsMeOutSection />
-    </Lenis>
-  );
+    console.log(portfolio);
+
+
+    return (
+        <Lenis root>
+            <HeroHeader/>
+            <HeroSection/>
+            <StaticsSection/>
+            <FeaturedProjectsSection/>
+            <TechStackSection/>
+        </Lenis>
+    );
 }
