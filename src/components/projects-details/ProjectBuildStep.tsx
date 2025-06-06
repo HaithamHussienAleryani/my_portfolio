829560cd508d1e8e7c6b30387592ccfb011b2afd
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-07 00:04:39 +0300

    add the build step component to the project details page

--- src/components/projects-details/ProjectBuildStep.tsx
+++ src/components/projects-details/ProjectBuildStep.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProjectBorderedHeader from "./ProjectBorderedHeader";
 
 function ProjectBuildStep() {
-  return <div>ProjectBuildStep</div>;
+  return (
+    <section className="mt-14">
+      <ProjectBorderedHeader header="Build Steps" />
+    </section>
+  );
 }
 
 export default ProjectBuildStep;
