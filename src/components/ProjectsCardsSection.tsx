0bb4e494da66520d01dae689abeafaf0103ba0da
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-11 19:30:21 +0300

    fix projects spacing

--- src/components/ProjectsCardsSection.tsx
+++ src/components/ProjectsCardsSection.tsx
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils";
 function ProjectsCardsSection({ projects }: { projects: any[] }) {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   return (
-    <div className="py-5 mt-5">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 md:gap-40 mx-auto">
+    <div className="py-5 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 md:gap-30 mx-auto">
         {projects.map((project, index) => (
           <div
             key={project.title}
