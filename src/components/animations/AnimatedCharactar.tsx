3dec9151a97c004d63906582a4f7127b11e7bba9
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-26 03:04:51 +0300

    about me animtaion

--- src/components/animations/AnimatedCharactar.tsx
+++ src/components/animations/AnimatedCharactar.tsx
@@ -1,76 +1,32 @@
 "use client";
+import { useGSAP } from "@gsap/react";
 import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
+import gsap from "gsap/all";
 import React, { useRef } from "react";
 
 export default function Paragraph({ paragraph }: { paragraph: string }) {
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start 0.9", "start 0.25"],
+  useGSAP(() => {
+    gsap.to(".animated-text", {
+      scrollTrigger: {
+        trigger: ".animated-text",
+        start: "top 80%",
+        end: "bottom 20%",
+        scrub: 1,
+      },
+      opacity: 1,
+      y: 0,
+      stagger: 0.1,
+      ease: "power1.out",
+    });
   });
-
-  const words = paragraph.split(" ");
   return (
     <p
-      ref={container} 
       style={{ lineHeight: 1 }}
       className={
-        "flex text-xl md:text-3xl font-bold justify-center leading-none p-10 max-w-screen-xl flex-wrap mx-auto"
+        " text-xl animated-text md:text-3xl text-center opacity-30 font-bold  leading-none p-10 max-w-screen-xl  mx-auto"
       }
     >
-      {words.map((word, i) => {
-        const start = i / words.length;
-        const end = start + 1 / words.length;
-        return (
-          <Word key={i} progress={scrollYProgress} range={[start, end]}>
-            {word}
-          </Word>
-        );
-      })}
+      {paragraph}
     </p>
   );
 }
-
-const Word = ({
-  children,
-  progress,
-  range,
-}: {
-  children: string;
-  progress: MotionValue;
-  range: number[];
-}) => {
-  const amount = range[1] - range[0];
-  const step = amount / children.length;
-  return (
-    <span className={"relative mr-1 md:mr-2 mt-2 md:mt-3"}>
-      {children.split("").map((char, i) => {
-        const start = range[0] + i * step;
-        const end = range[0] + (i + 1) * step;
-        return (
-          <Char key={`c_${i}`} progress={progress} range={[start, end]}>
-            {char}
-          </Char>
-        );
-      })}
-    </span>
-  );
-};
-
-const Char = ({
-  children,
-  progress,
-  range,
-}: {
-  children: string;
-  progress: MotionValue;
-  range: number[];
-}) => {
-  const opacity = useTransform(progress, range, [0, 1]);
-  return (
-    <span>
-      <span className={"absolute opacity-20 "}>{children}</span>
-      <motion.span style={{ opacity: opacity }}>{children}</motion.span>
-    </span>
-  );
-};
