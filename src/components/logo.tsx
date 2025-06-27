27cc29a9a7d541b595c103e0053700a70a82280d
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-27 20:55:00 +0300

    fix logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,8 +2,10 @@ import { cn } from "@/lib/utils";
 
 export const Logo = ({ className }: { className?: string }) => {
   return (
-    <h3 className={cn(className, "text-2xl font-bold font-work-sans")}>
-      H|<span className="text-primary font-normal">H</span>
+    <h3
+      className={cn(className, "text-2xl select-none font-bold font-work-sans")}
+    >
+      H|<span className="text-primary select-none ">H</span>
     </h3>
   );
 };
