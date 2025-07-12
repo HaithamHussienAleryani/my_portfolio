99b5254ed79b3ba0ca97850f1923ca51dba0e8ab
haithamaleryani <haithamaleryani00@gmail.com>
2025-07-13 00:44:24 +0300

    edit metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,24 +31,14 @@ export const metadata: Metadata = {
   description: "Please click the link and check out my portfolio",
   metadataBase: new URL("https://haithm.dev"), // <--- ADD THIS LINE
   openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://haithm.dev",
-    images: {
-      url: "/opengraph-image.png", // This will now resolve to https://haithm.dev/opengraph-image.png
-      width: 1200,
-      height: 630,
-      secureUrl: "https://haithm.dev/opengraph-image.png",
-      alt: "My Website Open Graph Image",
-      type: "image/png", // Optional but good practice to explicitly state the type
-    },
+    images: ["opengraph-image.png"],
   },
   twitter: {
     card: "summary_large_image",
+    site: "haithm.dev",
     title: "Haitham Hussien | Senior Front-End Developer",
     description: "Please click the link and check out my portfolio",
-
-    images: ["/opengraph-image.png"],
+    images: ["opengraph-image.png"],
   },
 };
 
